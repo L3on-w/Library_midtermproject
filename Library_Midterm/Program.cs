6df354661d4584b3c299660a8ecdf0040705731d
Leon <leonwilke1991@gmail.com>
2022-01-31 19:23:47 -0500

    minor change added

--- Library_Midterm/Program.cs
+++ Library_Midterm/Program.cs
@@ -7,6 +7,7 @@ namespace Library_Midterm
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Midterm");
         }
     }
 }
