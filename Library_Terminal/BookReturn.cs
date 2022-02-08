2289146b0e5e8310663e8136267485c55dfd869d
[MrRiceGuy] <carsonkeene@gmail.com>
2022-02-07 21:32:18 -0500

    Deletions and additions

--- Library_Terminal/BookReturn.cs
+++ Library_Terminal/BookReturn.cs
@@ -2,17 +2,17 @@
 
 public class BookReturn
 {
-    public BookReturn(string userTurnIn)
-    {
-        if (userTurnIn = "yes")
-        {
-            return "Thank you! Feel free to browse for your next read!"
-        }
-        else
-        {
-            return "Dont miss your due date!"
+    //public BookReturn(string userTurnIn)
+    //{
+    //    if (userTurnIn = "yes")
+    //    {
+    //        return "Thank you! Feel free to browse for your next read!";
+    //    }
+    //    else
+    //    {
+    //        return "Dont miss your due date!";
 
-        }
+    //    }
 
-    }
+    //}
 }
--- Library_Terminal/PullingTxtFile.cs
+++ Library_Terminal/PullingTxtFile.cs
@@ -1,54 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
 
-public class PullingFromtxt
-{
-    private object bookInfo;
+//public class PullingFromtxt
+//{
+//    private object bookInfo;
 
-    public PullingFromtxt()
-	{
-	    string[] PullFromTxt = File.ReadAllLines("../../../BookNames.txt");
+//    public PullingFromtxt()
+//    {
+//        string[] PullFromTxt = File.ReadAllLines("../../../BookNames.txt");
 
-		var bookTxtGrouping = PullFromTxt.GroupBy(b => b.Split('|')[1], b => b.Split('|')[0]);
+//        var bookTxtGrouping = PullFromTxt.GroupBy(b => b.Split('|')[1], b => b.Split('|')[0]);
 
-		var bookTxtClasses = new List<BookTxtPull>();
-		foreach (var bookinfo in PullFromTxt)
-        {
-			var splitBookInfo = bookinfo.Split('|');
-			var bookTitle = splitBookInfo[0];
-			var authorName = splitBookInfo[1];
+//        var bookTxtClasses = new List<BookTxtPull>();
+//        foreach (var bookinfo in PullFromTxt)
+//        {
+//            var splitBookInfo = bookinfo.Split('|');
+//            var bookTitle = splitBookInfo[0];
+//            var authorName = splitBookInfo[1];
 
-        }
+//        }
 
-        Console.WriteLine("Enter an Author to search their literature.");
-		foreach(var authorBooks in bookTxtClasses.Where(x => x, Author == authorName).Select(bookTxtClasses => bookTxtClasses.Title))
-
-
+//        Console.WriteLine("Enter an Author to search their literature.");
+//        foreach (var authorBooks in bookTxtClasses.Where(x => x, Author == authorName).Select(bookTxtClasses => bookTxtClasses.Title))
 
 
-	}
-}
 
-public class AuthorTxtPull
-{
-	public AuthorTxtPull(string authorName)
-	{
-		Author = authorName;
-	}
-	public string Author { get; set; } = string.Empty;
 
-}
 
-public class BookTxtPull
-{
-	public BookTxtPull(string bookTitle)
-	{
-		Title = bookTitle;
-	}
-	public string Title { get; set; } = string.Empty;
+//    }
+//}
 
-	public IEnumerable<string> BookTitles { get; set; } = new List<string>();
-}
+//public class AuthorTxtPull
+//{
+//    public AuthorTxtPull(string authorName)
+//    {
+//        Author = authorName;
+//    }
+//    public string Author { get; set; } = string.Empty;
+
+//}
+
+//public class BookTxtPull
+//{
+//    public BookTxtPull(string bookTitle)
+//    {
+//        Title = bookTitle;
+//    }
+//    public string Title { get; set; } = string.Empty;
+
+//    public IEnumerable<string> BookTitles { get; set; } = new List<string>();
+//}
 
